a3d6cdbec547b34fb6f400fc4c5f68a0718368a3
hypen-emdash <joffe.daniel@gmail.com>
2019-11-04 08:57:29 +0000

    Linted models.py and seed.py

--- mvg/seed.py
+++ mvg/seed.py
@@ -7,72 +7,79 @@ from database.models import AssetClass, Asset, Value
 
 Datapoint = namedtuple('Datapoint', 'date open close lo hi')
 
+
 def main():
     clear_tables()
     populate_asset_classes()
     populate_assets()
     populate_values()
 
+
 def clear_tables():
     AssetClass.objects.all().delete()
     Asset.objects.all().delete()
     Value.objects.all().delete()
 
+
 def populate_asset_classes():
     class_list = [
-    'Equity', 'Fixed Income', 'Cryptocurrency', 'Commodity',
+        'Equity', 'Fixed Income', 'Cryptocurrency', 'Commodity',
         'Foreign Exchange'
     ]
-    
+
     for name in class_list:
         AssetClass(name=name).save()
+
 
 def populate_assets():
     equities = AssetClass.objects.get(name='Equity').asset_set
     bonds = AssetClass.objects.get(name='Fixed Income').asset_set
     commodities = AssetClass.objects.get(name='Commodity').asset_set
     cryptocurrencies = AssetClass.objects.get(name='Cryptocurrency').asset_set
-    
+
     equity_list = [
         ('Dow Jones', 'DOW'),
         ('S&P 500 Index', 'S&P500'),
         ('NASDAQ NMS Composite Index', 'NASDAQ'),
     ]
-    
+
     bond_list = [
         ('Core US Aggregate Bond ETF', 'AGG'),
         ('Vanguard Total Bond Market', 'BND'),
         ('Investment Grate Corporate Bond', 'LQD'),
     ]
-    
+
     commodity_list = [
         ('Shrimp', 'Shrimp'),
         ('Sugar', 'Sugar'),
         ('Tea', 'Tea'),
     ]
-    
+
     cryptocurrency_list = [
         ('Bitcoin', 'BTC'),
         ('Etherium', 'ETH'),
         ('Ripple', 'XRP'),
     ]
-    
+
     assets = [
         (equities, equity_list),
         (bonds, bond_list),
         (commodities, commodity_list),
         (cryptocurrencies, cryptocurrency_list),
     ]
-    
+
     for asset in assets:
         fill_asset_set(asset[0], asset[1])
+
 
 def fill_asset_set(asset_set, source_list):
         for item in source_list:
             asset_set.create(name=item[0], abbreviation=item[1])
 
+
 def populate_values():
     populate_dow()
+
 
 def populate_dow():
     dow_data = Asset.objects.get(abbreviation='DOW').value_set
@@ -84,9 +91,10 @@ def populate_dow():
             low_price=datum.lo,
             high_price=datum.hi
         )
-        
+
         if i & 255 == 255:
             print(i)
+
 
 def read_dow():
     with open('../Financial Data/Equities/Dow Jones.csv') as src:
@@ -98,7 +106,7 @@ def read_dow():
             high_price = Decimal(row[2])
             low_price = Decimal(row[3])
             close_price = Decimal(row[4])
-            
+
             yield Datapoint(date, open_price, close_price, low_price, high_price)
 
 
