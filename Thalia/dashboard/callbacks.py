809808e2c97ab44bc75b0cf3ba6d23a8bcceeaf6
Martti <martti@aukia.com>
2020-01-22 17:14:58 +0000

    add basic graphing and table to dashboard
    uses fake data and hardcoded table values
    but in general i'ts ready for real data
    still needs to be refactored

--- Thalia/dashboard/callbacks.py
+++ Thalia/dashboard/callbacks.py
@@ -1,16 +1,67 @@
 from datetime import datetime as dt
 
 import pandas_datareader as pdr
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
+
+from . import layout
+
+
+def create_time_series(dff, title, x, y):
+    return dict(x=x, y=y, mode="lines+markers", name=title)
+
+
+def get_data(tickers):
+    return pdr.get_data_yahoo(tickers, start=dt(2017, 1, 1), end=dt.now())
 
 
 def register_callbacks(dashapp):
-    @dashapp.callback(Output("my-graph", "figure"), [Input("my-dropdown", "value")])
-    def update_graph(selected_dropdown_value):
-        df = pdr.get_data_yahoo(
-            selected_dropdown_value, start=dt(2017, 1, 1), end=dt.now()
-        )
-        return {
-            "data": [{"x": df.index, "y": df.Close}],
-            "layout": {"margin": {"l": 40, "r": 0, "t": 20, "b": 30}},
-        }
+    @dashapp.callback(
+        Output("graph-container", "children"),
+        [Input("submit-btn", "n_clicks")],
+        [
+            State("ticker1", "value"),
+            State("ticker2", "value"),
+            State("ticker3", "value"),
+            State("ticker1-proportion", "value"),
+            State("ticker2-proportion", "value"),
+            State("ticker3-proportion", "value"),
+        ],
+    )
+    def update_dashboard(
+        n_clicks, ticker1, ticker2, ticker3, ticker1_prop, ticker2_prop, ticker3_prop
+    ):
+        if n_clicks is None:
+            raise PreventUpdate
+        tickers = [dropdown for dropdown in (ticker1, ticker2, ticker3) if dropdown]
+        dfs = [get_data(ticker) for ticker in tickers]
+        table_data = get_table_data()
+        table_columns = [{"name": i, "id": i} for i in table_data[0].keys()]
+        return [
+            layout.create_graph(get_figure_data(tickers, dfs), "graph"),
+            layout.create_table(table_data, table_columns, "table"),
+        ]
+
+
+def get_table_data():
+    return [
+        {"metric": "Initial Balance", "value": 100},
+        {"metric": "End Balance", "value": 120},
+        {"metric": "Best Year", "value": 0.141},
+        {"metric": "Worst Year", "value": 0.141},
+        {"metric": "Sortino Ratio", "value": 176.158},
+        {"metric": "Sharpe Ratio", "value": 0.0057},
+        {"metric": "Max Drawdown", "value": 24.944},
+    ]
+
+
+def get_figure_data(tickers, dfs):
+    traces = [
+        create_time_series(df, name, df.index, df.Close)
+        for name, df in zip(tickers, dfs)
+    ]
+    figure = {
+        "data": traces,
+        "layout": {"margin": {"l": 40, "r": 0, "t": 20, "b": 30}},
+    }
+    return figure
--- Thalia/dashboard/layout.py
+++ Thalia/dashboard/layout.py
@@ -1,19 +1,32 @@
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
+
+
+def create_graph(figure, id):
+    return dcc.Graph(figure=figure, id=id)
+
+
+def create_table(data, columns, id):
+    return dash_table.DataTable(id=id, columns=columns, data=data)
+
+
+temp_tickers = [{"label": "Coke", "value": "COKE"},
+                {"label": "Tesla", "value": "TSLA"},
+                {"label": "Apple", "value": "AAPL"}]
 
 layout = html.Div(
     [
         html.H1("Stock Tickers"),
-        dcc.Dropdown(
-            id="my-dropdown",
-            options=[
-                {"label": "Coke", "value": "COKE"},
-                {"label": "Tesla", "value": "TSLA"},
-                {"label": "Apple", "value": "AAPL"},
-            ],
-            value="COKE",
-        ),
-        dcc.Graph(id="my-graph"),
+        dcc.Dropdown(id="ticker1", options=temp_tickers),
+        dcc.Input(id="ticker1-proportion", type='number', min=0, max=100),
+        dcc.Dropdown(id="ticker2", options=temp_tickers),
+        dcc.Input(id="ticker2-proportion", type='number', min=0, max=100),
+        dcc.Dropdown(id="ticker3", options=temp_tickers),
+        dcc.Input(id="ticker3-proportion", type='number', min=0, max=100),
+        html.Button('Submit', 'submit-btn'),
+        html.Div(id="graph-container"),
+        html.Div(id="table-container"),
     ],
     style={"width": "500"},
 )
