5012f8fb66204e95a8e3c75f48322f69716b073f
hypen-emdash <joffe.daniel@gmail.com>
2020-01-22 12:05:53 +0000

    Generated stubs.

--- analyse_data/analyse_data.py
+++ analyse_data/analyse_data.py
@@ -1,1 +1,22 @@
-# TODO: write code
+def total_return(strat):
+    pass
+
+
+def sortino_ratio(strat):
+    pass
+
+
+def sharpe_ratio(strat):
+    pass
+
+
+def max_drawdown(strat):
+    pass
+
+
+def best_year(strat):
+    pass
+
+
+def worst_year(strat):
+    pass
