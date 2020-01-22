b556ae17a8f0f6ff33ac40720461ba6e7a20ea83
hypen-emdash <joffe.daniel@gmail.com>
2020-01-22 13:44:31 +0000

    Added return type-hints.

--- analyse_data/analyse_data.py
+++ analyse_data/analyse_data.py
@@ -1,22 +1,27 @@
-def total_return(strat):
+from decimal import Decimal
+
+decimal.getcontext().prec = 2
+
+
+def total_return(strat) -> {date: Decimal}:
     pass
 
 
-def sortino_ratio(strat):
+def sortino_ratio(strat) -> float:
     pass
 
 
-def sharpe_ratio(strat):
+def sharpe_ratio(strat) -> float:
     pass
 
 
-def max_drawdown(strat):
+def max_drawdown(strat) -> Decimal:
     pass
 
 
-def best_year(strat):
+def best_year(strat) -> float:
     pass
 
 
-def worst_year(strat):
+def worst_year(strat) -> float:
     pass
