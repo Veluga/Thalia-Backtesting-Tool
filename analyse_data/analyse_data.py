bd32fbe3d10bb5010b6963c75f7c72065178f57b
hypen-emdash <joffe.daniel@gmail.com>
2020-01-23 11:37:26 +0000

    Added Strategy class to code.

--- analyse_data/analyse_data.py
+++ analyse_data/analyse_data.py
@@ -5,6 +5,28 @@ from decimal import Decimal
 decimal.getcontext().prec = 2
 
 
+class Strategy:
+    def __init__(
+        self,
+        start_date: datetime.date,
+        end_date: datetime.date,
+        starting_balance: decimal.Decimal,
+        assets: List[dict],
+        contribution_interval: datetime.timedelta,
+        contribution_amount: decimal.Decimal,
+        rebalancing_interval: datetime.timedelta,
+        risk_free_rate: pd.DataFrame,
+    ):
+        self.start_date = start_date
+        self.end_date = end_date
+        self.starting_balance = starting_balance
+        self.assets = assets
+        self.contribution_interval = contribution_interval
+        self.contribution_amount = contribution_amount
+        self.rebalancing_interval = rebalancing_interval
+        self.risk_free_rate = risk_free_rate
+
+
 def total_return(strat) -> pd.Series:
     # Returns the value of the portfolio at each day in the time frame.
     pass
