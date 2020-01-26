f050e954b2aaadba22ceb6985aaf2679094ba5b5
Arhtur-Louis Heath <a.heath.17@aberdeen.ac.uk>
2020-01-26 12:54:11 +0000

    Changes reviewed by hyphen-emdash and vejni

--- dbSchema.sql
+++ dbSchema.sql
@@ -1,16 +1,14 @@
 CREATE TABLE AssetClass (
-    ID INTEGER PRIMARY KEY,
-    Name TEXT
+    AssetClassName TEXT PRIMARY KEY
 );
 
 CREATE TABLE Asset (
-  ID INTEGER PRIMARY KEY,
-  Name TEXT,
-  AssetTicker TEXT UNIQUE,
-  AssetClass TEXT
+    AssetTicker TEXT PRIMARY KEY,
+    Name TEXT,
+    AssetClassName TEXT
 );
 
-CREATE TABLE AssetValues (
+CREATE TABLE AssetValue (
     ID INTEGER PRIMARY KEY,
     AssetTicker TEXT,
     ADate TEXT,
@@ -18,8 +16,8 @@ CREATE TABLE AssetValues (
     IsInterpolated INTEGER
 );
 
-CREATE INDEX IX_Asset_AssetClass ON Asset(AssetClass);
-CREATE INDEX UX_Values_AssetTicker ON AssetValues(AssetTicker);
-CREATE INDEX IX_Values_IsInterpolated ON AssetValues(IsInterpolated);
+CREATE INDEX IX_Asset_AssetClass ON Asset(AssetClassName);
+CREATE INDEX UX_Values_AssetTicker ON AssetValue(AssetTicker);
+CREATE INDEX IX_Values_IsInterpolated ON AssetValue(IsInterpolated);
 
 
