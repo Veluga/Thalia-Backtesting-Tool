9f84e84972f327a56657e9918c42afffad12ae47
Martti <martti@aukia.com>
2020-01-29 23:34:42 +0000

    update dashboard
    1. added graph and table to default layout
    and made callback update their figure and data respectively instead.
    Change made to better align with dash documentation.
    Reverting the change may become necessary in the future
    if updating more than just 1 prop
    is necessary
    for nicer organization
    
    2. changed figure to only show one graph
    for now by summing choices
    in retrospect this was unnecessary as
    the graph will rely on data from BL library
    
    3. Added bulma as CSS library for project, for easy responsive design
    
    also moved code around and added some todos

--- Thalia/dashboard/callbacks.py
+++ Thalia/dashboard/callbacks.py
@@ -1,23 +1,14 @@
 from datetime import datetime as dt
+from functools import reduce
 
 import pandas_datareader as pdr
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 
-from . import layout
-
-
-def create_time_series(dff, title, x, y):
-    return dict(x=x, y=y, mode="lines+markers", name=title)
-
-
-def get_data(tickers):
-    return pdr.get_data_yahoo(tickers, start=dt(2017, 1, 1), end=dt.now())
-
 
 def register_callbacks(dashapp):
-    @dashapp.callback(
-        Output("graph-container", "children"),
+    dashapp.callback(
+        [Output("graph", "figure"), Output("table", "data")],
         [Input("submit-btn", "n_clicks")],
         [
             State("ticker1", "value"),
@@ -27,23 +18,38 @@ def register_callbacks(dashapp):
             State("ticker2-proportion", "value"),
             State("ticker3-proportion", "value"),
         ],
-    )
-    def update_dashboard(
-        n_clicks, ticker1, ticker2, ticker3, ticker1_prop, ticker2_prop, ticker3_prop
-    ):
-        if n_clicks is None:
-            raise PreventUpdate
-        tickers = [dropdown for dropdown in (ticker1, ticker2, ticker3) if dropdown]
-        dfs = [get_data(ticker) for ticker in tickers]
-        table_data = get_table_data()
-        table_columns = [{"name": i, "id": i} for i in table_data[0].keys()]
-        return [
-            layout.create_graph(get_figure_data(tickers, dfs), "graph"),
-            layout.create_table(table_data, table_columns, "table"),
-        ]
+    )(update_dashboard)
+
+
+# TODO: make input and output dynamic, currently only supports 3
+# see this discussion for more info: https://community.plot.ly/t/dynamic-controls-and-dynamic-output-components/5519
+# GOAL is to have the UI support selection and distribution of arbitary numbers of assets
+def update_dashboard(
+    n_clicks, ticker1, ticker2, ticker3, ticker1_prop, ticker2_prop, ticker3_prop
+):
+    """
+    based on selected tickers and assets generate a graph of portfolios value over time
+    and a table of key metrics
+
+    TODO: make ticker selection and proportion selection matter
+    """
+    if n_clicks is None:
+        raise PreventUpdate
+    all_tickers = (ticker1, ticker2, ticker3)
+    # TODO: add error handling (UI facing message) for erronous input
+    tickers = [dropdown for dropdown in all_tickers if dropdown]
+    dfs = [get_data(ticker) for ticker in tickers]
+    # TODO: add error handling for ticker not found
+    porftolio_sum = reduce(lambda x, y: x.add(y), dfs)
+    dfs
+    table_data = get_table_data()  # TODO: make interactive
+    return get_figure_data(porftolio_sum), table_data
 
 
 def get_table_data():
+    """
+    return a list of key metrics and their values
+    """
     return [
         {"metric": "Initial Balance", "value": 100},
         {"metric": "End Balance", "value": 120},
@@ -55,13 +61,20 @@ def get_table_data():
     ]
 
 
-def get_figure_data(tickers, dfs):
+def get_figure_data(df):
     traces = [
-        create_time_series(df, name, df.index, df.Close)
-        for name, df in zip(tickers, dfs)
+        create_time_series(df, df.index, df.Close)
     ]
     figure = {
         "data": traces,
         "layout": {"margin": {"l": 40, "r": 0, "t": 20, "b": 30}},
     }
     return figure
+
+
+def create_time_series(dff, x, y):
+    return dict(x=x, y=y, mode="lines+markers")
+
+
+def get_data(tickers):
+    return pdr.get_data_yahoo(tickers, start=dt(2017, 1, 1), end=dt.now())
--- Thalia/dashboard/layout.py
+++ Thalia/dashboard/layout.py
@@ -3,48 +3,95 @@ import dash_html_components as html
 import dash_table
 
 
-def create_graph(figure, id):
+def graph(figure, id):
+    """
+    exists purely to be called from callbacks
+
+    TODO: evaluate if antipattern? wrong type of abstraction?
+    """
     return dcc.Graph(figure=figure, id=id)
 
 
-def create_table(data, columns, id):
+def table(data, id):
+    """
+    exists purely to be called from callbacks
+
+    TODO: evaluate if antipattern? wrong type of abstraction?
+    """
+    columns = [
+        {"name": "metric", "id": "metric"},
+        {"name": "value", "id": "value"},
+    ]
     return dash_table.DataTable(id=id, columns=columns, data=data)
 
 
-temp_tickers = [
-    {"label": "Coke", "value": "COKE"},
-    {"label": "Tesla", "value": "TSLA"},
-    {"label": "Apple", "value": "AAPL"},
-]
-
-
-def create_ticker_selector(id):
+def ticker_selector(id):
+    tickers = [
+        {"label": "Coke", "value": "COKE"},
+        {"label": "Tesla", "value": "TSLA"},
+        {"label": "Apple", "value": "AAPL"},
+    ]
     return html.Div(
         [
-            dcc.Dropdown(id=id, options=temp_tickers, className="two-thirds column"),
-            dcc.Input(
-                id=f"{id}-proportion",
-                type="number",
-                min=0,
-                max=100,
-                className="one-third column",
+            html.Div(
+                html.Div(
+                    [
+                        html.Label("Ticker: ", className="label"),
+                        html.Div(
+                            dcc.Dropdown(id=id, options=tickers, className=""),
+                            className="control",
+                        ),
+                    ],
+                    className="field",
+                ),
+                className="column",
             ),
+            html.Div(
+                html.Div(
+                    [
+                        html.Label("Allocation: ", className="label"),
+                        html.Div(
+                            dcc.Input(
+                                id=f"{id}-proportion",
+                                type="number",
+                                min=0,
+                                max=100,
+                                className="input",
+                            ),
+                            className="control",
+                        ),
+                    ],
+                    className="field",
+                ),
+                className="column",
+            ),
+        ],
+        className="columns is-marginless ",
+    )
+
+
+def options():
+    return html.Div(
+        [
+            html.Div(
+                [ticker_selector(f"ticker{i}") for i in range(1, 4)],
+                className="container",
+            ),
+            html.Button("Submit", "submit-btn", className="button is-large is-primary"),
         ],
         className="container",
     )
 
 
 layout = html.Div(
-    [
-        html.H1("Stock Tickers"),
-        html.Div(
-            [
-                html.Div([create_ticker_selector(f"ticker{i}") for i in range(1, 4)]),
-                html.Button("Submit", "submit-btn"),
-            ],
-        ),
-        html.Div(id="graph-container"),
-        html.Div(id="table-container"),
-    ],
-    style={"width": "500"},
+    html.Div(
+        [
+            html.H1("Stock Tickers", className="title"),
+            options(),
+            dcc.Loading(
+                [graph({}, id="graph"), table([], "table")], className="container"
+            ),
+        ],
+        className="section",
+    )
 )
