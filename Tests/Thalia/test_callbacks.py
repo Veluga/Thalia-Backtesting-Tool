04edde041204394eb835ac876b5e3338e0ea6c78
Martti <martti@aukia.com>
2020-01-30 19:36:44 +0000

    update callback test

--- Tests/Thalia/test_callbacks.py
+++ Tests/Thalia/test_callbacks.py
@@ -1,6 +1,7 @@
 from Thalia.dashboard import callbacks
 import pytest
 from dash.exceptions import PreventUpdate
+import plotly.graph_objects as go
 
 
 def test_update_dashboard_prevents_update():
@@ -13,7 +14,6 @@ def test_update_dashboard():
     tickers = ("AAPL", "TSLA", None)
     proportions = (60, 40, None)
     fig, table_data = callbacks.update_dashboard(1, *tickers, *proportions)
-    assert fig.get("data") is not None, "figures should have data"
-    assert fig.get("layout") is not None, "figures should have a layout"
+    assert isinstance(fig, go.Figure), "update dashboard should return a plotly figure"
     # FIXME: below test is probably too tied to implementation
     assert all(row.get("metric") and row.get("value") for row in table_data)
