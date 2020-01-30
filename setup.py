b42518f0be66c16671fefa45d66b2d5ee2f147ab
Martti <martti@aukia.com>
2020-01-30 01:13:31 +0000

    add setup.py file
    moved dependency responsibility to
    setup.py.
    Although basic app can still be installed with requirements.txt
    
    see readme for instructions on running

---
+++ setup.py
@@ -0,0 +1,28 @@
+"""A setuptools based setup module."""
+from os import path
+
+from setuptools import find_packages, setup
+
+here = path.abspath(path.dirname(__file__))
+
+install_requires = [
+    "flask",
+    "flask_assets",
+    "pandas",
+    "dash",
+    "pathlib",
+]
+
+
+tests_require = ["pytest", "coverage"]
+
+extras_require = {"dev": ["black", "flake8"], "test": tests_require}
+
+setup(
+    name="Thalia",
+    version="0.2.0",
+    packages=find_packages(),
+    install_requires=install_requires,
+    entry_points={"console_scripts": ["run = wsgi:main"]},
+    extras_require=extras_require,
+)
