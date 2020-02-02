cfdb11a8c10cd7624b5583eb490802dc97ab0e20
veronicakkv <55283210+veronicakkv@users.noreply.github.com>
2020-02-02 18:21:26 +0000

    added ON DELETE CASCADE

--- Documentation/dbSchema.sql
+++ Documentation/dbSchema.sql
@@ -5,7 +5,9 @@ CREATE TABLE AssetClass (
 CREATE TABLE Asset (
     AssetTicker TEXT PRIMARY KEY,
     Name TEXT,
-    AssetClassName TEXT
+    AssetClassName TEXT,
+    FOREIGN KEY(AssetClassName) REFERENCES AssetClass(AssetClassName)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE AssetValue (
@@ -16,12 +18,12 @@ CREATE TABLE AssetValue (
     ALow TEXT,
     AHigh TEXT,
     IsInterpolated INTEGER,
+    FOREIGN KEY(AssetTicker) REFERENCES Asset(AssetTicker) ON DELETE CASCADE
     PRIMARY KEY (AssetTicker, ADate)
+
 );
 
 CREATE INDEX IX_Asset_AssetClass ON Asset(AssetClassName);
 CREATE INDEX UX_Values_AssetTicker ON AssetValue(AssetTicker);
 CREATE INDEX UX_Value_Date_AssetTicker ON AssetValue(AssetTicker, ADate);
 CREATE INDEX IX_Values_IsInterpolated ON AssetValue(IsInterpolated);
-
-
