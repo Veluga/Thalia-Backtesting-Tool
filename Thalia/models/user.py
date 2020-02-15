7e9d8e88177119ae73f12b726e90dc13212a5360
Martti <martti@aukia.com>
2020-02-15 20:02:03 +0000

    rename id to user_id
    shadows builtin function. Not my favorite name user_id
    but it's still better than shadowing an existing function

--- Thalia/models/user.py
+++ Thalia/models/user.py
@@ -4,8 +4,8 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 
 @login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class User(UserMixin, db.Model):
