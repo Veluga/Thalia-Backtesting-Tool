1b9b9718460cda156bdcb907585b1e7eea8587e1
Martti <martti@aukia.com>
2020-02-15 19:55:33 +0000

    refactor dashboard updating
    seperate user input filtering and business logic calls
    
    also add a couple todos

--- Thalia/dashboard/callbacks.py
+++ Thalia/dashboard/callbacks.py
@@ -39,18 +39,46 @@ def update_dashboard(
     """
     if n_clicks is None:
         raise PreventUpdate
-    all_tickers = (ticker1, ticker2, ticker3)
+
     # TODO: add error handling (UI facing message) for erronous input
-    tickers = [dropdown for dropdown in all_tickers if dropdown]
-    dfs = get_data("")
+    all_tickers = (ticker1, ticker2, ticker3)
+    all_proportions = (ticker1_prop, ticker2_prop, ticker3_prop)
+
+    tickers, proportions = filter_dropdowns(all_tickers, all_proportions)
+    return update_backtest_results(tickers, proportions)
+
+
+def filter_dropdowns(tickers, proportions):
+    """
+    remove any ticker, proportion combos without a ticker selected
+
+    TODO: maybe return a strategy object instead?
+          currently does maybe a bit too much zipping and unzipping
+    """
+    tickers_with_prop = zip(tickers, proportions)
+    tickers_with_prop = [
+        dropdown for dropdown in tickers_with_prop if dropdown[0]
+    ]  # remove empty dropdowns without a ticker
+    tickers, proportions = zip(*tickers_with_prop)  # seperate tickers and props again
+    return tickers, proportions
+
+
+def update_backtest_results(tickers, proportion):
+    """
+    get timeseries and key metrics data for portfolio
+    """
     # TODO: add error handling for ticker not found
-    table_data = get_table_data()  # TODO: make interactive
-    return get_figure(dfs), table_data
+    assets_data = get_data(tickers)
+    # TODO: do anda stuff here
+    table_data = get_table_data()
+    return get_figure(assets_data), table_data
 
 
 def get_table_data():
     """
     return a list of key metrics and their values
+
+    TODO: add anda support here
     """
     return [
         {"metric": "Initial Balance", "value": 100},
@@ -74,6 +102,11 @@ def get_trace(x, y):
 
 
 def get_data(ticker):
+    """
+    retrive ticker data from database
+
+    TODO: add Finda support
+    """
     import numpy as np
 
     date_rng = pd.date_range(start="1/1/2010", end="1/08/2010", freq="H")
