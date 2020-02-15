504b6c0f8d28b46db61e354b38db54b14749df6f
george_stoian <george_stoian@protonmail.ch>
2020-02-15 02:56:32 +0200

    started working on the implementation

--- Harvester/DataHarvester/dh_fd_conn.py
+++ Harvester/DataHarvester/dh_fd_conn.py
@@ -1,13 +1,50 @@
 import sys
+import os
+import pandas as pd
 sys.path.append('../..')
 from  Finda import FdMultiController
-import pandas as pd
+
+
+def return_asset_classes():
+    
+    basepath = "tickers/"
+    array_list = []
+    for entry in os.listdir(basepath):
+        if os.path.isfile(os.path.join(basepath, entry)):
+            entry = entry.replace("_tickers.csv","")
+            array_list.append(entry)
+
+    return array_list
 
 '''
-
+    The following comments go from Intern to Wary Wizard.
+    Can the Wary Wizard verify if what the intern is treating
+    Finda in the way she deserves to be treated ?
+    In addition to this people like comments, or so I heard.
 '''
-FdMultiController.fd_create('first_db')
+'''
+    The Intern creates the database and then he connects to it.
+'''
+
+# ask if db exists first
+#FdMultiController.fd_create('first_db')
 conn = FdMultiController.fd_connect('first_db', 'rw')
 
+'''
+    Look into the tickers folder and based on the tickers there
+    create the list of asset classes
+'''
+asset_classes_list = []
+asset_classes_list = return_asset_classes()
 
-retrieved = conn.write.write_asset_classes()
+'''
+    Create a pandas.DataFrame of the format:
+    {Columns: [], Index: [AssetClassName<String>]}
+    Insert one at a time.
+'''
+
+
+for entry in asset_classes_list:
+    print(entry)
+    data_frame_asset_classes = pd.DataFrame( columns = [], index= ["index_funds"] )
+    conn.write_asset_classes(data_frame_asset_classes)
