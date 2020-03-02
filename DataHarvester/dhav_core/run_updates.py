1862f3db3bedc29f7a3f020c9409c1c872a97026
george_stoian <george_stoian@protonmail.ch>
2020-03-02 23:09:26 +0000

    small changes to the configuration

--- DataHarvester/dhav_core/run_updates.py
+++ DataHarvester/dhav_core/run_updates.py
@@ -13,8 +13,8 @@ def run_me():
 
     path = os.path.join(path,"apis_access")
 
-    api_calls_api1 = 10
-    api_calls_api2 = 10
+    api_calls_api1 = 2
+    api_calls_api2 = 2
     api1 = apic("yfinance", ["bonds", "comodities_future", "index_funds"], api_calls_api1)
     api2 = apic("nomics", ["crypto", "currency"], api_calls_api2, path, True)
 
