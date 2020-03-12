16b2ff708690e9d96f1a78df20c4648817df8ea2
Veluga <albertboehm@hotmail.de>
2020-03-12 17:20:50 +0000

    Added model for storing and retrieving portfolios.

---
+++ Thalia/models/portfolio.py
@@ -0,0 +1,16 @@
+from analyse_data.analyse_data import Strategy
+from ..extensions import db
+import pickle
+
+class Portfolio(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    strategy = db.Column(db.BLOB(), nullable=False)
+
+    def __repr__(self):
+        return "<Portfolio {} {}>".format(self.id, len(self.strategy))
+
+    def set_strategy(self, strat):
+        self.strategy = pickle.dumps(strat)
+    
+    def get_strategy(self):
+        return pickle.loads(self.strategy)
