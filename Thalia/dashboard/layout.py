2b75b59c6cd7d9c1d6cc3d561768495aed741781
Martti <martti@aukia.com>
2020-03-14 13:23:15 +0000

    add ui only upload box to dashboard
    UI only, no backend logic

--- Thalia/dashboard/layout.py
+++ Thalia/dashboard/layout.py
@@ -31,6 +31,23 @@ def ticker_selector():
     )
 
 
+def upload_box():
+    return dcc.Upload(
+        id="upload-data",
+        children=html.Div(["Drag and Drop or ", html.A("Select Files")]),
+        style={
+            "width": "100%",
+            "height": "60px",
+            "lineHeight": "60px",
+            "borderWidth": "1px",
+            "borderStyle": "dashed",
+            "borderRadius": "5px",
+            "textAlign": "center",
+            "margin": "10px",
+        },
+    )
+
+
 def ticker_dropdown(dropdown_items):
     return html.Div(
         html.Div(
@@ -47,6 +64,7 @@ def ticker_dropdown(dropdown_items):
                     ],
                     className="control",
                 ),
+                upload_box()
             ],
             className="field",
         ),
