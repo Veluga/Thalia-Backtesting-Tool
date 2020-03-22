26d70a4050afaf6c056c3b0098e679fce045c250
veronicakkv <55283210+veronicakkv@users.noreply.github.com>
2020-03-22 15:55:25 +0000

    options stored in seperate file

---
+++ Thalia/dashboard/tab_elements/lazy_portfolio.py
@@ -0,0 +1,50 @@
+lazy_portfolio_options = [
+    {"label": "None", "value": None},
+    {
+        "label": "Stocks/Bonds (60/40)",
+        "value": """
+    {'Asset1': {'AssetTicker': 'VTSMX', 'Allocation': '60'},
+    'Asset2': {'AssetTicker': 'VBMFX', 'Allocation': '40'}}
+    """,
+    },
+    {
+        "label": "Growth Portfolio",
+        "value": """
+    {'Asset1': {'AssetTicker': 'VTSMX', 'Allocation': '52'},
+    'Asset2': {'AssetTicker': 'VGTSX', 'Allocation': '28'},
+    'Asset3': {'AssetTicker': 'VBMFX', 'Allocation': '16'},
+    'Asset4': {'AssetTicker': 'PIGLX', 'Allocation': '4'}}
+    """,
+    },
+    {
+        "label": "Bogleheads Four Funds",
+        "value": """
+    {'Asset1': {'AssetTicker': 'VTSMX', 'Allocation': '50'},
+    'Asset2': {'AssetTicker': 'VGTSX', 'Allocation': '30'},
+    'Asset3': {'AssetTicker': 'VBMFX', 'Allocation': '10'},
+    'Asset4': {'AssetTicker': 'VIPSX', 'Allocation': '10'}}
+    """,
+    },
+    {
+        "label": "Ray Dalio All Weather",
+        "value": """
+    {'Asset1': {'AssetTicker': 'VTI', 'Allocation': '30'},
+    'Asset2': {'AssetTicker': 'TLT', 'Allocation': '40'},
+    'Asset3': {'AssetTicker': 'IEF', 'Allocation': '15'},
+    'Asset4': {'AssetTicker': 'DBC', 'Allocation': '7.50'},
+    'Asset5': {'AssetTicker': 'GLD', 'Allocation': '7.50'}}
+    """,
+    },
+    {
+        "label": "S&P 500",
+        "value": """
+    {'Asset1': {'AssetTicker': 'SPY', 'Allocation': '100'}}
+    """,
+    },
+    {
+        "label": "Total World Equities",
+        "value": """
+    {'Asset1': {'AssetTicker': 'VT', 'Allocation': '100'}}
+    """,
+    },
+]
