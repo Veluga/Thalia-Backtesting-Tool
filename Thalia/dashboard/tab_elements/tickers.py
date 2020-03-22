cb46840af2bc45684746e3987e731df65237e60d
veronicakkv <55283210+veronicakkv@users.noreply.github.com>
2020-03-22 15:54:39 +0000

    Update tickers.py

--- Thalia/dashboard/tab_elements/tickers.py
+++ Thalia/dashboard/tab_elements/tickers.py
@@ -2,6 +2,7 @@ import dash_html_components as html
 import dash_core_components as dcc
 import dash_table
 from datetime import datetime as dt
+from . import lazy_portfolio
 from .. import util
 
 
@@ -171,65 +172,17 @@ def rebalancing_dates(id):
     )
 
 
-def lazy_portfolios(id):
-    # stocks_bonds = {
-    # "Asset1": {"AssetTicker": "VTSMX", "Allocation": "60"},
-    # "Asset2": {"AssetTicker": "VBMFX", "Allocation": "40"},
-    # }
 
-    return dcc.Dropdown(
-        id=f"lazy-portfolios-{id}",
-        className="has-text-left",
-        options=[
-            {"label": "None", "value": "None"},
-            {
-                "label": "Stocks/Bonds (60/40)",
-                "value": """
-                {'Asset1': {'AssetTicker': 'VTSMX', 'Allocation': '60'},
-                'Asset2': {'AssetTicker': 'VBMFX', 'Allocation': '40'}}
-                """,
-            },
-            {
-                "label": "Growth Portfolio",
-                "value": """
-                {'Asset1': {'AssetTicker': 'VTSMX', 'Allocation': '52'},
-                'Asset2': {'AssetTicker': 'VGTSX', 'Allocation': '28'},
-                'Asset3': {'AssetTicker': 'VBMFX', 'Allocation': '16'},
-                'Asset4': {'AssetTicker': 'PIGLX', 'Allocation': '4'}}
-                """,
-            },
-            {
-                "label": "Bogleheads Four Funds",
-                "value": """
-                {'Asset1': {'AssetTicker': 'VTSMX', 'Allocation': '50'},
-                'Asset2': {'AssetTicker': 'VGTSX', 'Allocation': '30'},
-                'Asset3': {'AssetTicker': 'VBMFX', 'Allocation': '10'},
-                'Asset4': {'AssetTicker': 'VIPSX', 'Allocation': '10'}}
-                """,
-            },
-            {
-                "label": "Ray Dalio All Weather",
-                "value": """
-                {'Asset1': {'AssetTicker': 'VTI', 'Allocation': '30'},
-                'Asset2': {'AssetTicker': 'TLT', 'Allocation': '40'},
-                'Asset3': {'AssetTicker': 'IEF', 'Allocation': '15'},
-                'Asset4': {'AssetTicker': 'DBC', 'Allocation': '7.50'},
-                'Asset5': {'AssetTicker': 'GLD', 'Allocation': '7.50'}}
-                """,
-            },
-            {
-                "label": "S&P 500",
-                "value": """
-                {'Asset1': {'AssetTicker': 'SPY', 'Allocation': '100'}}
-                """,
-            },
-            {
-                "label": "Total World Equities",
-                "value": """
-                {'Asset1': {'AssetTicker': 'VT', 'Allocation': '100'}}
-                """,
-            },
-        ],
+def lazy_portfolios(id):
+    return (
+        html.Div(
+            dcc.Dropdown(
+                id=f"lazy-portfolios-{id}",
+                placeholder="Lazy portfolio",
+                className="has-text-left",
+                options=lazy_portfolio.lazy_portfolio_options,
+            ),
+        ),
     )
 
 
