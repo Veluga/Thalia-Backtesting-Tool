41c2767bd5521b2e738dd5e0ab132371de19f505
george_stoian <george_stoian@protonmail.ch>
2020-04-13 16:08:54 +0100

    added js script

---
+++ Thalia/static/scripts/export-pdf.js
@@ -0,0 +1,17 @@
+function exportPDF(){
+    var doc = new jsPDF();
+    var elementHTML = document.body.innerHTML;
+    var specialElementHandlers = {
+        '#notThis': function (element, renderer) {
+            return true;
+    }
+    };
+    doc.fromHTML(elementHTML, 15, 15, {
+        'width': 170,
+        'elementHandlers': specialElementHandlers
+    });
+
+    // Save the PDF
+doc.save('sample-document.pdf');
+
+}
