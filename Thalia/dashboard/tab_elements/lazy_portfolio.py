bedc20ede39965184748bfe3cab9ce0f93c27e9c
Martti <martti@aukia.com>
2020-04-15 18:14:03 +0300

    add names to lazy portfolio tickers

--- Thalia/dashboard/tab_elements/lazy_portfolio.py
+++ Thalia/dashboard/tab_elements/lazy_portfolio.py
@@ -3,48 +3,48 @@ lazy_portfolio_options = [
     {
         "label": "Stocks/Bonds (60/40)",
         "value": """
-    {'Asset1': {'AssetTicker': 'VTSMX', 'Allocation': '60'},
-    'Asset2': {'AssetTicker': 'VBMFX', 'Allocation': '40'}}
+    {'Asset1': {'AssetTicker': 'VTSMX', 'Name': 'Vanguard TSM Idx', 'Allocation': '60'},
+    'Asset2': {'AssetTicker': 'VBMFX', 'Name': 'Vanguard Total Bond Market Index Fund Investor Shares', 'Allocation': '40'}}
     """,
     },
     {
         "label": "Growth Portfolio",
         "value": """
-    {'Asset1': {'AssetTicker': 'VTSMX', 'Allocation': '52'},
-    'Asset2': {'AssetTicker': 'VGTSX', 'Allocation': '28'},
-    'Asset3': {'AssetTicker': 'VBMFX', 'Allocation': '16'},
-    'Asset4': {'AssetTicker': 'PIGLX', 'Allocation': '4'}}
+    {'Asset1': {'AssetTicker': 'VTSMX', 'Name': 'Vanguard TSM Idx','Allocation': '52'},
+    'Asset2': {'AssetTicker': 'VGTSX', 'Name': 'Vanguard Tot I S', 'Allocation': '28'},
+    'Asset3': {'AssetTicker': 'VBMFX', 'Name': 'Vanguard Total Bond Market Index Fund Investor Shares','Allocation': '16'},
+    'Asset4': {'AssetTicker': 'PIGLX', 'Name': 'PIMCO Global Bond Opportunities ', 'Allocation': '4'}}
     """,
     },
     {
         "label": "Bogleheads Four Funds",
         "value": """
-    {'Asset1': {'AssetTicker': 'VTSMX', 'Allocation': '50'},
-    'Asset2': {'AssetTicker': 'VGTSX', 'Allocation': '30'},
-    'Asset3': {'AssetTicker': 'VBMFX', 'Allocation': '10'},
-    'Asset4': {'AssetTicker': 'VIPSX', 'Allocation': '10'}}
+    {'Asset1': {'AssetTicker': 'VTSMX', 'Name': 'Vanguard TSM Idx','Allocation': '50'},
+    'Asset2': {'AssetTicker': 'VGTSX', 'Name': 'Vanguard Tot I S','Allocation': '30'},
+    'Asset3': {'AssetTicker': 'VBMFX', 'Name': 'Vanguard Total Bond Market Index Fund Investor Shares','Allocation': '10'},
+    'Asset4': {'AssetTicker': 'VIPSX', 'Name': 'Vanguard Inflation-Protected Securities ', 'Allocation': '10'}}
     """,
     },
     {
         "label": "Ray Dalio All Weather",
         "value": """
-    {'Asset1': {'AssetTicker': 'VTI', 'Allocation': '30'},
-    'Asset2': {'AssetTicker': 'TLT', 'Allocation': '40'},
-    'Asset3': {'AssetTicker': 'IEF', 'Allocation': '15'},
-    'Asset4': {'AssetTicker': 'DBC', 'Allocation': '7.50'},
-    'Asset5': {'AssetTicker': 'GLD', 'Allocation': '7.50'}}
+    {'Asset1': {'AssetTicker': 'VTI', 'Name': 'Vanguard TSM Idx', 'Allocation': '30'},
+    'Asset2': {'AssetTicker': 'TLT', 'Name': 'iShares 20+ Year Treasury Bond ETF', 'Allocation': '40'},
+    'Asset3': {'AssetTicker': 'IEF', 'Name': 'iShares 7-10 Year Treasury Bond ETF', 'Allocation': '15'},
+    'Asset4': {'AssetTicker': 'DBC', 'Name': 'PowerShares DB Com', 'Allocation': '7.50'},
+    'Asset5': {'AssetTicker': 'GLD', 'Name': 'Gold Trust', 'Allocation': '7.50'}}
     """,
     },
     {
         "label": "S&P 500",
         "value": """
-    {'Asset1': {'AssetTicker': 'SPY', 'Allocation': '100'}}
+    {'Asset1': {'AssetTicker': 'SPY', 'Name': 'SPDR S&P 500 ETF', 'Allocation': '100'}}
     """,
     },
     {
         "label": "Total World Equities",
         "value": """
-    {'Asset1': {'AssetTicker': 'VT', 'Allocation': '100'}}
+    {'Asset1': {'AssetTicker': 'VT', 'Name': 'VANGUARD INTL E/TOTAL WORLD' 'Allocation': '100'}}
     """,
     },
 ]
